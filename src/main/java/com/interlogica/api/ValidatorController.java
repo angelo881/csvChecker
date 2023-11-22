bbdc83a06065cdbee534864b8dbc591bc129a970
amezzina <a.mezzina@mariniimpianti.it>
2023-11-22 12:45:44 +0100

    wip test + form verifica numero

--- src/main/java/com/interlogica/api/ValidatorController.java
+++ src/main/java/com/interlogica/api/ValidatorController.java
@@ -7,23 +7,42 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.interlogica.business.ValidatorSourceManager;
+import com.interlogica.data.AfricanPhoneNumber;
 import com.interlogica.data.RowResult;
 
-@RestController
+@Controller
 public class ValidatorController {
 
 	@Autowired
 	private ValidatorSourceManager service;
 
 	@PostMapping("/validateNumber")
-	ResponseEntity<RowResult> validateNumber(@RequestParam String phone_number) {
+	public ResponseEntity<RowResult> validateNumber(@RequestParam String phone_number) {
 		return ResponseEntity.ok(service.validate(UUID.randomUUID().toString(), phone_number));
+	}
+
+	@GetMapping("/")
+	public String showForm(Model model) {
+		AfricanPhoneNumber african_number = new AfricanPhoneNumber();
+		model.addAttribute("african_number", african_number);
+
+		return "validation_form";
+	}
+
+	@PostMapping("/validateNumberWeb")
+	public String validateNumberWeb(Model model, @ModelAttribute("african_number") AfricanPhoneNumber number) {
+		model.addAttribute("report", service.validate(UUID.randomUUID().toString(), number.getNumber()));
+
+		return "validation_report";
 	}
 
 	@PostMapping("/consumeFile")
--- src/test/java/com/interlogica/api/ValidatorControllerTest.java
+++ src/test/java/com/interlogica/api/ValidatorControllerTest.java
@@ -12,9 +12,11 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.interlogica.business.ValidatorSourceManager;
+import com.interlogica.data.AfricanPhoneNumber;
 
 @ExtendWith(MockitoExtension.class)
 public class ValidatorControllerTest {
@@ -33,18 +35,32 @@ public class ValidatorControllerTest {
 	}
 
 	@Test
+	public void testValidateWeb() {
+		String number = "32231232";
+		controller.validateNumberWeb(Mockito.mock(Model.class), new AfricanPhoneNumber(number));
+		Mockito.verify(service).validate(Mockito.anyString(), Mockito.eq(number));
+	}
+
+	@Test
+	public void testShowForm() {
+		String res = controller.showForm(Mockito.mock(Model.class));
+		assertEquals("validation_form", res);
+	}
+
+	@Test
 	public void testLoadFile() throws IOException {
 		MultipartFile mock = Mockito.mock(MultipartFile.class);
 		ResponseEntity<?> resp = controller.consumeFile(mock);
 		Mockito.verify(service).loadFile(mock);
 		assertEquals(HttpStatus.OK, resp.getStatusCode());
 	}
+
 	@Test
 	public void testLoadFileKO() throws IOException {
 		MultipartFile mock = Mockito.mock(MultipartFile.class);
 		Mockito.when(service.loadFile(mock)).thenThrow(RuntimeException.class);
 		ResponseEntity<?> resp = controller.consumeFile(mock);
-		
+
 		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, resp.getStatusCode());
 	}
 }
